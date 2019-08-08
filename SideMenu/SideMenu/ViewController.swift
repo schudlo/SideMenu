ca9e157d0c84d9e7fcce7dee3a11b095e632db84
Andrii Shchudlo <andrii.shchudlo@likims.com>
2019-08-08 15:27:03 +0200

    [Master] - initialized project

---
+++ SideMenu/SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Andrii Shchudlo on 08/08/2019.
+//  Copyright © 2019 Andrii Shchudlo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
